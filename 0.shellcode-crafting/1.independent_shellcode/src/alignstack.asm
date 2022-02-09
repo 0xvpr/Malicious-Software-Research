ede1eb6869e2501b66ea59c759b0fdd3135bbf5c
0xvpr <malikrb@live.com>
2022-02-09 10:05:38 -0600

    README.md improved; alignstack.asm modified

--- 0.shellcode-crafting/1.independent_shellcode/src/alignstack.asm
+++ 0.shellcode-crafting/1.independent_shellcode/src/alignstack.asm
@@ -8,7 +8,9 @@ alignstack:
     mov     rdi, rsp            ; Save the stack pointer to rdi
     and     rsp, byte -0x10     ; Align the stack with 16 bytes
     sub     rsp, byte +0x20     ; Allocate space for the write_hello function
+
     call    write_hello         ; Call to write_hello
     mov     rsp, rdi            ; Restore stack pointer
+
     pop     rdi                 ; Restore rdi
     ret                         ; Return back to entry
