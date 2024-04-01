28a11ca60356d9155ddc68b803ad6224678faa5e
0xvpr <malikrb@live.com>
2024-04-01 12:03:54 -0400

    Improved syscall.asm

--- 00.shellcode-crafting/03-pure-assembly-syscall/src/syscall.asm
+++ 00.shellcode-crafting/03-pure-assembly-syscall/src/syscall.asm
@@ -1,51 +1,43 @@
 global          entry
-global          NtWriteFile
-global          KrnlGetStdHandle
 
-section         .text
-value:
-    dq          -11
+section         .rodata
 isb:
     dd          0
     dq          0
 str:
     db          "Syscall gang",0xA,0
+
+section         .text
 entry:
     push        rbp
     mov         rbp, rsp
-    sub         rsp, 0x78
-
-    mov         ecx, dword [rel value]
-    call        KrnlGetStdHandle
-    push        rax
-
+    sub         rsp, 0x48
+    mov         rcx, [gs:0x60]                      ; PEB
+    cmp         byte [rcx + 0x3], 1
+    je          exit
+prologue:
     xor         eax, eax
-    mov         qword [rsp + 0x48], rax
     mov         qword [rsp + 0x40], rax
-    mov         qword [rsp + 0x38], entry - str     ; entry-str = strlen
-    lea         rax, [rel str]
-    mov         qword [rsp + 0x30], rax
-    lea         rax, [rel isb]
+    mov         qword [rsp + 0x38], rax
+    mov         qword [rsp + 0x30], 13              ; strlen(str) == 13
+    lea         rax, qword [rel str]
     mov         qword [rsp + 0x28], rax
+    lea         rax, qword [rel isb]
+    mov         qword [rsp + 0x20], rax
     xor         r9d, r9d
     xor         r8d, r8d
     xor         edx, edx
-    pop         rcx
-    call        NtWriteFile
 
+    mov         rcx, [rcx + 0x20]                   ; PEB loader
+    mov         rcx, [rcx + 0x28]                   ; kernel32
+    call        NtWriteFile
+exit:
     xor         eax, eax
-    add         rsp, 0x78
+    add         rsp, 0x48
     pop         rbp
     ret
-
 NtWriteFile:
-    mov         r10, rcx 
+    mov         r10, rcx
     mov         eax, 0x8                            ; 0x8 is the syscall for NtWriteFile
     syscall
-    ret
-
-KrnlGetStdHandle:
-    mov         rax, [gs:0x60]                      ; Loading in kernel32
-    mov         rcx, [rax + 0x20]                   
-    mov         rax, [rcx + 0x28]
     ret
